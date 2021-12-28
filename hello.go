0513d80907f5d7138034f018a513514b2ec1a7d0
Oleg Smedyuk <conformist.mw@gmail.com>
2021-12-28 15:04:00 +0200

    switch case for number range

--- hello.go
+++ hello.go
@@ -7,10 +7,16 @@ import (
 func main() {
 	var number int
 	fmt.Scan(&number)
-	var first, second, third int = number / 100, (number / 10) % 10, number % 10
-	if first != second && second != third && third != first {
-		fmt.Println("YES")
-	} else {
-		fmt.Println("NO")
+	switch {
+	case number == 10000:
+		fmt.Println(1)
+	case 10000 > number && number > 999:
+		fmt.Println(number / 1000)
+	case 1000 > number && number > 99:
+		fmt.Println(number / 100)
+	case 100 > number && number > 9:
+		fmt.Println(number / 10)
+	default:
+		fmt.Println(number)
 	}
 }
