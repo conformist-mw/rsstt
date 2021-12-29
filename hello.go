a0802049ea3dfc71ed3ad4b95477603489275e8c
Oleg Smedyuk <conformist.mw@gmail.com>
2021-12-29 18:58:00 +0200

    leap year

--- hello.go
+++ hello.go
@@ -2,21 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	var number int
-	fmt.Scan(&number)
-	var (
-		first int = number % int(math.Pow10(6)) / int(math.Pow10(5))
-		second int = number % int(math.Pow10(5)) / int(math.Pow10(4))
-		third int = number % int(math.Pow10(4)) / int(math.Pow10(3))
-		fourth int = number % int(math.Pow10(3)) / int(math.Pow10(2))
-		fifth int = number % int(math.Pow10(2)) / int(math.Pow10(1))
-		sixth int = number % int(math.Pow10(1)) / int(math.Pow10(0))
-	)
-	if (first + second + third) == (fourth + fifth + sixth) {
+	var year int
+	fmt.Scan(&year)
+	if year % 400 == 0 || year % 4 == 0 && year % 100 != 0 {
 		fmt.Println("YES")
 	} else {
 		fmt.Println("NO")
