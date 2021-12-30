9ae17d1f73ebe2a4ec47f15aea4440025fc26104
Oleg Smedyuk <conformist.mw@gmail.com>
2021-12-30 23:06:00 +0200

    for loop

--- hello.go
+++ hello.go
@@ -5,11 +5,7 @@ import (
 )
 
 func main() {
-	var year int
-	fmt.Scan(&year)
-	if year % 400 == 0 || year % 4 == 0 && year % 100 != 0 {
-		fmt.Println("YES")
-	} else {
-		fmt.Println("NO")
+	for i := 1; i <= 10; i++ {
+		fmt.Println(i * i)
 	}
 }
