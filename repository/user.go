25baec58e19d8a162840aadab788c4f2adb10beb
Oleg Smedyuk <oleg.smedyuk@gmail.com>
2024-08-22 22:14:09 +0300

    limit items only for the last day

--- repository/user.go
+++ repository/user.go
@@ -1,6 +1,10 @@
 package repository
 
-import "github.com/conformist-mw/rsstt/models"
+import (
+	"time"
+
+	"github.com/conformist-mw/rsstt/models"
+)
 
 func CreateUser(tg_chat_id string) {
 	user := models.User{
@@ -38,7 +42,9 @@ func GetUsers() []models.User {
 
 func GetUnseenItems(user_id uint) []models.Item {
 	var items []models.Item
-	models.DB.Where("subscriptions.user_id = ? AND seen_items.item_id IS NULL", user_id).
+	last24Hours := time.Now().Add(-24 * time.Hour)
+
+	models.DB.Where("subscriptions.user_id = ? AND seen_items.item_id IS NULL AND items.created_at >= ?", user_id, last24Hours).
 		Joins("JOIN feeds ON items.feed_id = feeds.id").
 		Joins("JOIN subscriptions ON feeds.id = subscriptions.feed_id").
 		Joins("LEFT OUTER JOIN seen_items ON items.id = seen_items.item_id AND seen_items.user_id = ?", user_id).
