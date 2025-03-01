2220af731604f017ca76f53aba87fc9081a3fb4f
Oleg Smedyuk <oleg.smedyuk@gmail.com>
2025-03-01 20:38:19 +0200

    try to resend simplified message on error

--- service/send.go
+++ service/send.go
@@ -29,51 +29,64 @@ func escapeMarkdown(text string) string {
 	return escaped
 }
 
-func generateMessage(item *models.Item) string {
+func generateMessageAndDescription(item *models.Item) (string, string) {
 	title := escapeMarkdown(*item.Title)
 	message := fmt.Sprintf("[%s](%s)", title, item.Link)
-
+	
 	var dateStr string
 	if item.PublishedAt != nil {
-		dateStr = fmt.Sprintf("Дата публикации: %s", item.PublishedAt.Format("2006-01-02 15:04:05"))
+		dateStr = fmt.Sprintf("Published: %s", item.PublishedAt.Format("2006-01-02"))
 	} else {
-		dateStr = fmt.Sprintf("Дата создания: %s", item.CreatedAt.Format("2006-01-02 15:04:05"))
+		dateStr = fmt.Sprintf("Created: %s", item.CreatedAt.Format("2006-01-02"))
 	}
 	message += "\n" + escapeMarkdown(dateStr)
-
+	description := ""
+	
 	if item.Description != nil {
 		remainingLength := 4096 - len(message) - 2
 		if remainingLength > 0 {
-			description := regexp.MustCompile("<[^>]*>").ReplaceAllString(*item.Description, "")
+			description = regexp.MustCompile("<[^>]*>").ReplaceAllString(*item.Description, "")
 			description = html.UnescapeString(description)
 			description = escapeMarkdown(description)
 
 			if len(description) > remainingLength {
 				description = description[:remainingLength-3] + "..."
 			}
-
-			message += "\n\n" + description
 		}
 	}
-	return message
+	return message, description
 }
 
 func SendItemToUser(item *models.Item, tgChatId string, botUrl string) error {
+	message, description := generateMessageAndDescription(item)
+	
+	fullMessage := message + "\n\n" + description
+	err := sendTelegramMessage(item.ID, tgChatId, fullMessage, botUrl)
+	
+	if err != nil {
+		logger.Log.Infof("Trying to send simplified message. Item: %d", item.ID)
+		err = sendTelegramMessage(item.ID, tgChatId, message, botUrl)
+	}
+	
+	return err
+}
+
+func sendTelegramMessage(itemID uint, chatID string, text string, botUrl string) error {
 	tgMessage := TelegramMessage{
-		ChatID:    tgChatId,
-		Text:      generateMessage(item),
+		ChatID:    chatID,
+		Text:      text,
 		ParseMode: "MarkdownV2",
 	}
-	logger.Log.Debugf("Sending telegram message. Item: %d, Message: %s", item.ID, tgMessage.Text)
+	logger.Log.Debugf("Sending telegram message. Item: %d, Message: %s", itemID, tgMessage.Text)
 	jsonData, err := json.Marshal(tgMessage)
 	if err != nil {
-		logger.Log.Errorf("Error marshalling telegram message. Item: %d, Error: %s", item.ID, err)
+		logger.Log.Errorf("Error marshalling telegram message. Item: %d, Error: %s", itemID, err)
 		return err
 	}
 
 	resp, err := http.Post(botUrl, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
-		logger.Log.Errorf("Error sending telegram message. Item: %d, Error: %s", item.ID, err)
+		logger.Log.Errorf("Error sending telegram message. Item: %d, Error: %s", itemID, err)
 		return err
 	}
 	defer resp.Body.Close()
@@ -81,9 +94,9 @@ func SendItemToUser(item *models.Item, tgChatId string, botUrl string) error {
 	if resp.StatusCode != http.StatusOK {
 		var result map[string]interface{}
 		if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-			logger.Log.Errorf("Failed to decode error response. Status code: %d, Item: %d, Error: %s", resp.StatusCode, item.ID, err)
+			logger.Log.Errorf("Failed to decode error response. Status code: %d, Item: %d, Error: %s", resp.StatusCode, itemID, err)
 		} else {
-			logger.Log.Errorf("Failed to send message. Status code: %d, Item: %d, Response: %v", resp.StatusCode, item.ID, result)
+			logger.Log.Errorf("Failed to send message. Status code: %d, Item: %d, Response: %v", resp.StatusCode, itemID, result)
 		}
 		return fmt.Errorf("failed to send message, status code: %d", resp.StatusCode)
 	}
